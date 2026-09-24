9363badee9be69a4f409401e3bc989e9ca79f62e
Sakib0017 <sakibkamal0017@gmail.com>
2026-09-24 20:57:59 +0600

    Commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: { port: 5173, proxy: { '/api': 'http://localhost:5000', '/uploads': 'http://localhost:5000' } },
-  build: { outDir: 'dist', sourcemap: false },
+  build: { outDir: 'dist', sourcemap: false, emptyOutDir: true },
   preview: { port: 4173 },
 });
